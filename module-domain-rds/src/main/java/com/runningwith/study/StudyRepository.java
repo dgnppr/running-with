0e9ae210f8a7c97a0cbd072f247e0d606644ef30
dragonappear <dragonappear1@gmail.com>
2023-02-20 14:35:45 +0900

    add: find study by path with entity graph

--- module-domain-rds/src/main/java/com/runningwith/study/StudyRepository.java
+++ module-domain-rds/src/main/java/com/runningwith/study/StudyRepository.java
@@ -1,7 +1,15 @@
 package com.runningwith.study;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
+@Transactional(readOnly = true)
 public interface StudyRepository extends JpaRepository<StudyEntity, Long> {
     boolean existsByPath(String path);
+
+    @EntityGraph(attributePaths = {"tags", "zones", "managers", "members"}, type = EntityGraph.EntityGraphType.LOAD)
+    Optional<StudyEntity> findByPath(String path);
 }
