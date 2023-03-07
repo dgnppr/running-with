0a67df7db1ff44aece195dec46cfe8bc290321ad
dragonappear <dragonappear1@gmail.com>
2023-03-08 00:46:33 +0900

    test: change test code

---
+++ module-application/runningwith-app-users-web/src/test/java/com/runningwith/infra/ContainerBaseTest.java
@@ -0,0 +1,12 @@
+package com.runningwith.infra;
+
+// TODO 테스트 컨테이너 적용
+public abstract class ContainerBaseTest {
+
+//    static final MariaDBContainer MARIA_DB_CONTAINER;
+//
+//    static {
+//        MARIA_DB_CONTAINER = new MariaDBContainer<>("mariadb:latest");
+//        MARIA_DB_CONTAINER.start();
+//    }
+}
