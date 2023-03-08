7cbe437b0cd377ef32216b10fd73999c076437a0
dragonappear <dragonappear1@gmail.com>
2023-03-09 04:15:46 +0900

    feat: unchecked notification for authenticated

---
+++ module-application/runningwith-app-users-web/src/main/java/com/runningwith/domain/notification/NotificationInterceptor.java
@@ -0,0 +1,38 @@
+package com.runningwith.domain.notification;
+
+import com.runningwith.domain.users.UsersContext;
+import com.runningwith.domain.users.UsersEntity;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
+
+import static com.runningwith.infra.utils.WebUtils.REDIRECT;
+
+@Component
+@RequiredArgsConstructor
+public class NotificationInterceptor implements HandlerInterceptor {
+
+    private final NotificationRepository notificationRepository;
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (modelAndView != null && !isRedirectView(modelAndView) && authentication != null && authentication.getPrincipal() instanceof UsersContext) {
+            UsersEntity usersEntity = ((UsersContext) authentication.getPrincipal()).getUsersEntity();
+            long count = notificationRepository.countByUsersEntityAndChecked(usersEntity, false);
+            modelAndView.addObject("hasNotification", count > 0);
+        }
+    }
+
+    private boolean isRedirectView(ModelAndView modelAndView) {
+        return modelAndView.getViewName().startsWith(REDIRECT) || modelAndView.getView() instanceof RedirectView;
+    }
+}
--- module-domain-rds/src/main/java/com/runningwith/domain/notification/NotificationRepository.java
+++ module-domain-rds/src/main/java/com/runningwith/domain/notification/NotificationRepository.java
@@ -1,6 +1,8 @@
 package com.runningwith.domain.notification;
 
+import com.runningwith.domain.users.UsersEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NotificationRepository extends JpaRepository<NotificationEntity, Long> {
+    long countByUsersEntityAndChecked(UsersEntity usersEntity, boolean checked);
 }
